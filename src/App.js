38f9d928cbfff9fe984725416a91f255fb843a68
tobsailbot <tobsailbot@gmail.com>
2023-03-31 01:36:40 -0300

    tipos de input en formulario

--- src/App.js
+++ src/App.js
@@ -10,32 +10,43 @@ function App() {
           </main> */}
         <div className="background">
           <div class='container border border-5 rounded p-5 text-start mt-5'>
-            <label class='dark h4 text-start'>Lugar</label>
-            <input type='text' placeholder='Escribir aquí' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4'>Fecha</label>
-            <input type='text' placeholder='Escribir aquí' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4'>Hora</label>
-            <input type='text' placeholder='Escribir aquí' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4'>Cantidad de jugadores</label>
-            <input type='number' placeholder='Escribir aquí' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4'>Precio</label>
-            <input type='number' placeholder='$' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4 text-start'>Nombre Equipo 1</label>
-            <input type='text' placeholder='Escribir aquí' class='form-control bg-dark text-light mb-3'></input>
-            
-            <label class='dark h4 text-start'>Nombre Equipo 2</label>
-            <input type='text' placeholder='Escribir aquí' class='form-control bg-dark mb-3'></input>
+            <div class='formulario border justify-content-center align-items-center text-left'>
 
+            <label class='dark h4 text-start'>Lugar</label>
+            <br></br>
+            <input type='text' placeholder='Escribir aquí' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4'>Fecha</label>
+            <br></br>
+            <input type='date' placeholder='Escribir aquí' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4'>Hora</label>
+            <br></br>
+            <input type='time' placeholder='Escribir aquí' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4'>Cantidad de jugadores</label>
+            <br></br>
+            <input type='number' placeholder='Escribir aquí' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4'>Precio</label>
+            <br></br>
+            <input type='number' placeholder='$' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4 text-start'>Nombre Equipo 1</label>
+            <br></br>
+            <input type='text' placeholder='Escribir aquí' class='form-control-lg bg-dark text-light mb-3'></input>
+            <br></br>
+            <label class='dark h4 text-start'>Nombre Equipo 2</label>
+            <br></br>
+            <input type='text' placeholder='Escribir aquí' class='form-control-lg bg-dark mb-3'></input>
+            <br></br>
             <input type='checkbox' class='bg-dark mb-5 form-check-input'></input>&nbsp; &nbsp;
             <label class='dark h4 pt-1'>Alquilado</label> 
             <br></br>
             
-          <button class='btn btn-dark btn-lg me-2 px-3'>Armar doparti</button>
+          <button type='submit' class='btn btn-dark btn-lg me-2 px-5'>Armar doparti</button>
+            </div>
+
             </div>
           
         </div>
